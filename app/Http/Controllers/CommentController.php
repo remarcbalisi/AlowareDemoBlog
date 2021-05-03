d08183bdde973180cde66270efb56e339167ebf8
Remarc Balisi <remarc.balisi@gmail.com>
2021-05-03 12:29:51 +0800

    sort latest

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -11,7 +11,7 @@ class CommentController extends Controller
 {
     public function index()
     {
-        return CommentResource::collection(Comment::with('parent')->paginate());
+        return CommentResource::collection(Comment::with('parent')->latest()->paginate());
     }
 
     public function store(CommentPostRequest $request)
